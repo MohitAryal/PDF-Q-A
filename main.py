bde5d4ed39152473166b4c49b260eb6295c1e7e9
MohitAryal <cozahitman@gmail.com>
2025-08-14 20:46:56 +0545

    implemented pdf loading

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 
 from dotenv import load_dotenv
 
 load_dotenv()
+
+loader = PyPDFLoader('sample_data/one.pdf', mode='page')
+
+docs = loader.load()
