0b3bb769e398f3ae95ff2f4ce0b1a83d9ec16852
MohitAryal <cozahitman@gmail.com>
2025-08-14 22:09:20 +0545

    Added prompt, llm and output generating codes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-loader = PyPDFLoader('sample_data/one.pdf', mode='page')
+loader = PyPDFLoader('sample_data/one.pdf', mode='single')
 docs = loader.load()
 
 splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
@@ -22,4 +22,17 @@ embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-
 vector_store = Chroma(collection_name='Pdf', embedding_function=embeddings, persist_directory='chroma')
 vector_store.add_documents(chunks)
 
-vector_store.invoke({'query': 'AI'})
+retriever = VectorStoreRetriever(vectorstore=vector_store)
+
+prompt = ChatPromptTemplate([
+    ('system', 'You are a helpful assistant responding to user queries using the following context. {context}'),
+    ('human', '{input}')
+])
+llm = ChatGroq(model="deepseek-r1-distill-llama-70b", temperature=0)
+
+doc_chain = create_stuff_documents_chain(llm=llm, prompt=prompt)
+retrieval_chain = create_retrieval_chain(retriever, doc_chain)
+
+response = retrieval_chain.invoke({'input': 'Which vector DB is user-friendly?'})
+clean_answer = re.sub(r"<think>.*?</think>", "", response['answer'], flags=re.DOTALL).strip()
+print(clean_answer)
