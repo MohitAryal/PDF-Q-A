9cf826bc54266961a005d5411586714c45bd5862
MohitAryal <cozahitman@gmail.com>
2025-08-14 21:27:47 +0545

    Added embeddings to chromadb

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_chroma import Chroma
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.vectorstores import VectorStoreRetriever
+from langchain_core.prompts import ChatPromptTemplate
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
 
 loader = PyPDFLoader('sample_data/one.pdf', mode='page')
-
 docs = loader.load()
 
 splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-texts = splitter.split_documents(docs)
+chunks = splitter.split_documents(docs)
 
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+vector_store = Chroma(collection_name='Pdf', embedding_function=embeddings, persist_directory='chroma')
+vector_store.add_documents(chunks)
+
+vector_store.invoke({'query': 'AI'})
