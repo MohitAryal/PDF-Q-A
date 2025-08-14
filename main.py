651a2cbc437ed95f5c38b62f37ae29da559f68e0
MohitAryal <cozahitman@gmail.com>
2025-08-14 20:23:00 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_groq import ChatGroq
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community import PyPDFLoader
+
+from dotenv import load_dotenv
+
+load_dotenv()
