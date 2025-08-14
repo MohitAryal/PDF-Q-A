2753cc755ff1d667eb196ee0fd1e21817a41bf9b
MohitAryal <cozahitman@gmail.com>
2025-08-14 21:09:29 +0545

    Added text splitter

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,3 +9,7 @@ load_dotenv()
 loader = PyPDFLoader('sample_data/one.pdf', mode='page')
 
 docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+texts = splitter.split_documents(docs)
+
